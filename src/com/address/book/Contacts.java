815c27cb87a244c4e5095a896b6275d7cf4cead7
unknown <khomanesuraj8@gmail.com>
2021-10-12 18:04:41 +0530

    create contacts in uc1

---
+++ src/com/address/book/Contacts.java
@@ -0,0 +1,29 @@
+package com.address.book;
+
+public class Contacts {
+
+	String firstName, lastName, address, city, state, zip,phoneNumber, email;
+
+	public Contacts(String firstName, String lastName, String address, String city, String state, String zip,
+			String phoneNumber, String email) {
+		super();
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.address = address;
+		this.city = city;
+		this.state = state;
+		this.zip = zip;
+		this.phoneNumber = phoneNumber;
+		this.email = email;
+	}
+
+	@Override
+	public String toString() {
+		return "Contacts [firstName=" + firstName + ", lastName=" + lastName + ", address=" + address + ", city=" + city
+				+ ", state=" + state + ", zip=" + zip + ", phoneNumber=" + phoneNumber + ", email=" + email + "]";
+	}
+	
+	
+	
+	
+}
