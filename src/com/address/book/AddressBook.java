69c09015894937ad2705e257db16c3a152d7ebf7
unknown <khomanesuraj8@gmail.com>
2021-10-14 15:44:00 +0530

    Added comments

--- src/com/address/book/AddressBook.java
+++ src/com/address/book/AddressBook.java
@@ -3,16 +3,17 @@ import java.util.Scanner;
 public class AddressBook {
 	
 	public static void main(String[] args) {
-		
 		// print message
 		System.out.println("Welcome to Address Book Program");
+		
 		//create object 
 		PersonInfo person = new PersonInfo();
-	    System.out.println("Address Book Menu!");
+	    
+		System.out.println("Address Book Menu!");
         System.out.println("Enter '1'to add person detail");//choices for operation
-        Scanner sc = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);		
         int choice = sc.nextInt();
-        
+        //swith case for choise
         switch (choice){
             case 1:
             	//add contact
