d1d3488b685dbf642e1b5ca7176db0bc9f839411
unknown <khomanesuraj8@gmail.com>
2021-10-14 15:37:39 +0530

    Added comments

--- src/com/address/book/AddressBook.java
+++ src/com/address/book/AddressBook.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class AddressBook {
 	
 	public static void main(String[] args) {
+		
 		// print message
 		System.out.println("Welcome to Address Book Program");
 		//create object 
