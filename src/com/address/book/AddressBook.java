f6152b99e3604c9da522c3a9cee88ff1c35cc7c9
unknown <khomanesuraj8@gmail.com>
2021-10-18 16:33:04 +0530

    Added: multiple person

--- src/com/address/book/AddressBook.java
+++ src/com/address/book/AddressBook.java
@@ -7,27 +7,35 @@ public class AddressBook {
 		System.out.println("Welcome to Address Book Program");
 		//create object
 		PersonInfo personDetail = new PersonInfo();
-		System.out.println("Enter 1.add person detail  2.Edit contact  3.Delete contact"); //choices for operation
-		Scanner sc = new Scanner(System.in);	
-		int choice = sc.nextInt();
-		//switch case for choice
-		switch (choice){
-		case 1:
-			//add person detail
-			personDetail.addContacts();
-			System.out.println("Contact Added Successfully!");
-			break;
-		case 2:
-			personDetail.editContacts();
-			System.out.println("Contact Edited Successfully!");
-			break;
-		case 3:
-			personDetail.deleteContact();
-			System.out.println("Contact delete Successfully!");
-			break;
-		default:
-			System.out.println("Please Enter Valid Choice:");
-			break;
+		boolean flag = true;
+		
+		while(flag) {
+			System.out.println("Enter 1.add person detail  2.Edit contact  3.Delete contact 4.Exit"); //choices for operation
+			Scanner sc = new Scanner(System.in);	
+			int choice = sc.nextInt();
+			//switch case for choice
+			switch (choice){
+			case 1:
+				//add person detail
+				personDetail.addContacts();
+				System.out.println("Contact Added Successfully!");
+				break;
+			case 2:
+				personDetail.editContacts();
+				System.out.println("Contact Edited Successfully!");
+				break;
+			case 3:
+				personDetail.deleteContact();
+				System.out.println("Contact delete Successfully!");
+				break;
+			case 4:
+				flag = false;
+				System.out.println("Exit");
+				break;
+			default:
+				System.out.println("Please Enter Valid Choice:");
+				break;
+		}
 		}
 	}
 }
